a33785df3eb624cee3f1aa78505ea909c3ab4a82
Svetasheva Yuliya <yusvetasheva@digital-spirit.ru>
2025-03-17 15:28:30 +0300

    нет изменения

--- src/main/java/com/example/courses/repository/CourseRepository.java
+++ src/main/java/com/example/courses/repository/CourseRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**/
 @Repository
 public interface CourseRepository extends JpaRepository<CourseEntity, Integer> {
     Optional<CourseEntity> findByName(String name);
